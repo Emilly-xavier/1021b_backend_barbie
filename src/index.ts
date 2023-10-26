1e0cbb0be9535948d9e4f0fcc67e90eba7edddb3
Emilly-xavier <emilly.xavier@estudante.ifms.edu.br>
2023-10-26 10:16:52 -0400

    Atualizando...

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,37 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 //Tenho que ter uma rota post para cadastrar um filme
+
+//Salvar em algum lugar o filme que foi cadastrado.
+type Filme = {
+    id:number,
+    titulo:string,
+    descricao:string,
+    imagem:string
+}
+let filmesCadastros:Filme[] = []
 app.post('/filmes',(req,res)=>{
+    const {id,titulo,descricao,imagem} = req.body
     const filme = {
-        id: 1,
-        titulo:'Vingadores',
-        descricao:'Filme dos herois da marvel',
-        imagem:"https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg"
+        id,
+        titulo,
+        descricao,
+        imagem
     }
+    //Como eu salvo o filme que foi cadastrado no meu vetor de filmes (Banco de dados)
+    filmesCadastros.push(filme)
     res.status(201).send(filme)
 })
 app.get('/filmes',(req,res)=>{
     res.send("Filmes Listados com sucesso")
 })
+
+app.get('/filmes/:id',(req,res)=>{
+    const id = parseInt(req.params.id)
+    //FIND para buscar um filme pelo id e retornar para o usuário
+    res.status(200).send(//Filme encontrado)
+})
+
 //Tenho que iniciar o servidor na porta 3000
 app.listen(3000,()=>{
     console.log('Servidor rodando na porta 3000')
