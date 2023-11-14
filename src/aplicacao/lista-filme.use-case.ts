4cd1c79844f5f25ed9da8c8dc4f0117a6e85e736
Emilly-xavier <emilly.xavier@estudante.ifms.edu.br>
2023-11-14 08:24:40 -0400

    novo commit...

---
+++ src/aplicacao/lista-filme.use-case.ts
@@ -0,0 +1,10 @@
+import FilmeRepositorioInterface from "./filme-repositorio-interface";
+
+export default class ListaFilme{
+    constructor (readonly filmeRepositorio:FilmeRepositorioInterface){
+
+    }
+    public async executar(){
+        return this.filmeRepositorio.listar()
+    }
+}
